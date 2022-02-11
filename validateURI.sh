5b232d488ef4914c5773678a96fef7713fb08266
SrikanthPandanaboina <46436387+SrikanthPandanaboina@users.noreply.github.com>
2022-02-11 12:13:50 +0530

    Update validateURI.sh

--- validateURI.sh
+++ validateURI.sh
@@ -8,6 +8,6 @@ else
   response=`curl -o /dev/null -s -w "%{http_code}\n" http://$URL`
 fi
 echo "The response code is: $response"
-if (( $response < 200 || $response > 400)); then
+if (( $response < '200' || $response > '400')); then
   exit 1
 fi
